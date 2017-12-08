d063cb27e1947f94ab9fbec0f56d5bd3af3fe752
Arkadiy Tetelman <arkadiyt@gmail.com>
2017-12-07 23:06:47 -0800

    Rework hackerone parsing

---
+++ lib/bounty-targets.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module BountyTargets
+end
+
+require 'bounty-targets/bugcrowd'
+require 'bounty-targets/cli'
+require 'bounty-targets/erb_context'
+require 'bounty-targets/hackerone'
--- lib/bounty-targets/hackerone.rb
+++ lib/bounty-targets/hackerone.rb
@@ -12,52 +12,116 @@ module BountyTargets
       return @scan_results if instance_variable_defined?(:@scan_results)
       schema # initialize graphql client
 
-      @scan_results = []
+      @scan_results = directory_index.map do |program|
+        in_scope, out_of_scope = program_targets(program)
+        program.merge(
+          'targets' => {
+            'in_scope' => in_scope,
+            'out_of_scope' => out_of_scope
+          }
+        )
+      end
+    end
 
-      after = nil
-      Kernel.loop do
-        page = @graphql_client.query(@query, variables: {after: after})
-        after = page.data.teams.page_info.end_cursor
-        nodes = page.data.teams.edges.map(&:node)
+    def directory_index
+      # Hackerone has ~5000 programs with ~200 signed up directly and the rest being
+      # community curated pages. The graphql fields that specify this information about a program
+      # are declared `!` (non-null), but hackerone returns them as null for some teams, causing
+      # graphql errors. Hackerone itself does not run into this error because they don't use the graphql
+      # endpoint to fetch the team directory, they use a different REST endpoint. We
+      # do the same here
 
-        if nodes.reject(&:nil?).length != nodes.length
-          raise StandardError, 'Some teams timed out'
-        end
+      uri = URI('https://hackerone.com/programs/search')
+      page = 1
+      programs = []
+      ::Kernel.loop do
+        uri.query = ::URI.encode_www_form(query: 'type:hackerone', sort: 'published_at:ascending', page: page)
+        result = ::JSON.parse(::Net::HTTP.get(uri))
+        page += 1
 
-        @scan_results.concat(nodes.map(&method(:convert_to_hash)))
+        programs.concat(result['results'].map do |program|
+          {
+            id: program['id'],
+            name: program['name'],
+            handle: program['handle'],
+            url: "https://hackerone.com#{program['url']}",
+            offers_bounties: program['meta']['offers_bounties'],
+            quick_to_bounty: program['meta']['quick_to_bounty'],
+            quick_to_first_response: program['meta']['quick_to_first_response']
+          }
+        end)
 
-        break unless page.data.teams.page_info.has_next_page
+        break if programs.size == result['total']
       end
 
-      @scan_results
+      programs
+    end
+
+    def program_targets(program)
+      scopes = []
+      after = nil
+
+      Kernel.loop do
+        page = @graphql_client.query(@query, variables: {handle: program[:handle], after: after})
+        after = page.data.team.structured_scopes.page_info.end_cursor
+        page_scopes = page.data.team.structured_scopes.edges
+
+        unless page.errors.details.empty?
+          raise StandardError, page.errors.details.to_s
+        end
+
+        raise StandardError, 'Some scopes timed out' if page_scopes.any?(&:nil?)
+        scopes.concat(page_scopes.map do |edge|
+          edge.node.to_h
+        end)
+
+        break unless page.data.team.structured_scopes.page_info.has_next_page
+      end
+
+      raise StandardError, 'Got duplicate scopes' if scopes.length != scopes.uniq.length
+
+      scopes = scopes.group_by do |scope|
+        scope['eligible_for_submission']
+      end.map do |key, targets|
+        [key, targets.sort_by do |scope|
+          scope['asset_identifier']
+        end]
+      end.to_h
+
+      [scopes.fetch(true, []), scopes.fetch(false, [])]
     end
 
     def schema
       return @schema if instance_variable_defined?(:@schema)
 
-      @http = GraphQL::Client::HTTP.new('https://hackerone.com/graphql')
-      @schema = GraphQL::Client.load_schema(@http)
-      @graphql_client = GraphQL::Client.new(schema: @schema, execute: @http)
+      @http = ::GraphQL::Client::HTTP.new('https://hackerone.com/graphql')
+      @schema = ::GraphQL::Client.load_schema(@http)
+      @graphql_client = ::GraphQL::Client.new(schema: @schema, execute: @http)
       @graphql_client.allow_dynamic_queries = true
 
-      # Trying to paginate more than 10 teams at a time causes results to be silently dropped
       @query = @graphql_client.parse <<~GRAPHQL
-        query($after: String) {
-          teams(first: 10, after: $after) {
-            pageInfo {
-              #{fields_for_type('PageInfo')},
-            },
-            total_count,
-            edges {
-              node {
-                #{fields_for_type('Team', %w[structured_scopes submission_state])},
-                structured_scopes(first: 100) {
-                  total_count,
-                  edges {
-                    node {
-                      #{fields_for_type('StructuredScope', %w[created_at])}
-                    }
-                  }
+        query($handle: String!, $after: String) {
+          team(handle: $handle) {
+            structured_scopes(first: 100, after: $after) {
+              pageInfo {
+                endCursor,
+                hasNextPage,
+                hasPreviousPage,
+                startCursor
+              },
+              total_count,
+              edges {
+                cursor,
+                node {
+                  asset_identifier,
+                  asset_type,
+                  availability_requirement,
+                  confidentiality_requirement,
+                  eligible_for_bounty,
+                  eligible_for_submission,
+                  instruction,
+                  integrity_requirement,
+                  max_severity
                 }
               }
             }
@@ -76,48 +140,6 @@ module BountyTargets
       end.map do |scope|
         scope['asset_identifier']
       end
-    end
-
-    private
-
-    def convert_to_hash(node)
-      node = node.to_h
-
-      result = node.slice('name', 'url', 'offers_bounties', 'offers_swag')
-
-      scopes = Array(node.dig('structured_scopes', 'edges'))
-      if scopes.length == 100
-        raise StandardError, 'Need to start paginating scopes'
-      end
-
-      scopes = scopes.map do |scope|
-        if scope.nil? || scope.empty?
-          raise StandardError, 'Some scopes timed out'
-        end
-
-        scope['node'].slice('asset_identifier', 'asset_type', 'availability_requirement',
-                            'confidentiality_requirement', 'eligible_for_bounty', 'eligible_for_submission',
-                            'instruction', 'integrity_requirement', 'max_severity')
-      end.group_by do |scope|
-        scope['eligible_for_submission']
-      end.map do |key, targets|
-        [key, targets.sort_by do |scope|
-          scope['asset_identifier']
-        end]
-      end.to_h
-
-      result.merge(
-        'targets' => {
-          'in_scope' => scopes.fetch(true, []),
-          'out_of_scope' => scopes.fetch(false, [])
-        }
-      )
-    end
-
-    def fields_for_type(type, exclude = [])
-      @schema.get_fields(@schema.types[type]).select do |key, field|
-        field.arguments.empty? && !exclude.include?(key)
-      end.keys.join(',')
     end
   end
 end
