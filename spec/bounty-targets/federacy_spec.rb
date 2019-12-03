1cc21dfc7006e8af01a936ef55aaf0fa6253e2b4
James Sulinski <james@federacy.com>
2019-12-03 07:03:29 +0000

    Fix tests for Federacy and remove old fixtures.

--- spec/bounty-targets/federacy_spec.rb
+++ spec/bounty-targets/federacy_spec.rb
@@ -11,29 +11,19 @@ describe BountyTargets::Federacy do
 
   it 'should fetch a list of programs' do
     programs = IO.read('spec/fixtures/federacy/programs.json')
-    stub_request(:get, %r{/api/programs}).with(headers: {host: 'one.federacy.com'})
+    stub_request(:get, %r{/api/public_programs}).with(headers: {host: 'www.federacy.com'})
       .to_return(status: 200, body: programs)
     expect(subject.directory_index).to eq(
       [
         {
           id: '955a3f33-3ca7-42b1-acf5-84da28d4c08c',
           name: 'Federacy',
-          url: 'https://api.federacy.com/api/programs/federacy'
-        },
-        {
-          id: 'f24e4ca8-2cd1-49ee-9467-77af68ae9cec',
-          name: 'Stacker',
-          url: 'https://api.federacy.com/api/programs/stacker'
+          url: 'https://www.federacy.com/federacy'
         },
         {
           id: '50cea250-a08a-4581-93a5-5d973a261f45',
           name: 'CoinTracker',
-          url: 'https://api.federacy.com/api/programs/cointracker'
-        },
-        {
-          id: '3ec4fbb9-d2df-4cc9-9cd6-7a1022b633d0',
-          name: 'Webflow',
-          url: 'https://api.federacy.com/api/programs/webflow'
+          url: 'https://www.federacy.com/cointracker'
         }
       ]
     )
@@ -41,29 +31,23 @@ describe BountyTargets::Federacy do
 
   it 'should fetch program scopes' do
     scopes = IO.read('spec/fixtures/federacy/scopes.json')
-    stub_request(:get, %r{/api/program_scopes})
-      .with(headers: {host: 'one.federacy.com'}, query: {program_id: '50cea250-a08a-4581-93a5-5d973a261f45'})
+    stub_request(:get, %r{/api/public_programs/50cea250-a08a-4581-93a5-5d973a261f45/program_scopes})
+      .with(headers: {host: 'www.federacy.com'})
       .to_return(status: 200, body: scopes)
     expect(subject.program_scopes(id: '50cea250-a08a-4581-93a5-5d973a261f45')).to eq(
       targets: {
         in_scope: [
           {
             type: 'website',
-            target: 'www.cointracker.io',
-            bounty: 'money',
-            impact: 'high'
+            target: 'www.cointracker.io'
           },
           {
             type: 'mobile app',
-            target: 'https://itunes.apple.com/us/app/cointracker-crypto-portfolio/id1401499763?mt=8',
-            bounty: 'money',
-            impact: 'high'
+            target: 'https://itunes.apple.com/us/app/cointracker-crypto-portfolio/id1401499763?mt=8'
           },
           {
             type: 'mobile app',
-            target: 'https://play.google.com/store/apps/details?id=io.cointracker.android',
-            bounty: 'money',
-            impact: 'high'
+            target: 'https://play.google.com/store/apps/details?id=io.cointracker.android'
           }
         ],
         out_of_scope: []
