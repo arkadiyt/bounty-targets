1f4773e9015174b6f40addb141dd681392a44cec
bounty-targets <>
2022-05-23 20:15:32 -0700

    Fix hackenproof rewards & spec

--- spec/bounty-targets/hackenproof_spec.rb
+++ spec/bounty-targets/hackenproof_spec.rb
@@ -14,116 +14,28 @@ describe BountyTargets::Hackenproof do
       body: File.read('spec/fixtures/hackenproof/programs_2.html'))
 
     expect(subject.directory_index).to eq(
-      [
-        {
-          id: '/allbridge/allbridge',
-          name: 'Allbridge',
-          url: 'https://hackenproof.com/allbridge/allbridge',
-          archived: false
-        },
-        {
-          id: '/avalanche/avalanche-general',
-          name: 'Avalanche General',
-          url: 'https://hackenproof.com/avalanche/avalanche-general',
-          archived: false
-        },
-        {
-          id: '/avalanche/avalanche-protocol',
-          name: 'Avalanche Protocol',
-          url: 'https://hackenproof.com/avalanche/avalanche-protocol',
-          archived: false
-        },
-        {
-          id: '/btse/btse-bug-bounty-program',
-          name: 'BTSE Bug Bounty Program',
-          url: 'https://hackenproof.com/btse/btse-bug-bounty-program',
-          archived: false
-        },
-        {
-          id: '/bitkub/bitkub',
-          name: 'Bitkub',
-          url: 'https://hackenproof.com/bitkub/bitkub',
-          archived: false
-        },
-        {
-          id: '/bunicorn/bunicorn-1',
-          name: 'Bunicorn',
-          url: 'https://hackenproof.com/bunicorn/bunicorn-1',
-          archived: false
-        },
-        {
-          id: '/coingecko/coingecko',
-          name: 'CoinGecko',
-          url: 'https://hackenproof.com/coingecko/coingecko',
-          archived: false
-        },
-        {
-          id: '/coinmetro/coinmetro-exchange',
-          name: 'CoinMetro Exchange',
-          url: 'https://hackenproof.com/coinmetro/coinmetro-exchange',
-          archived: false
-        },
-        {
-          id: '/coinsbit/coinsbit',
-          name: 'Coinsbit',
-          url: 'https://hackenproof.com/coinsbit/coinsbit',
-          archived: false
-        },
-        {
-          id: '/cryptology/cryptology',
-          name: 'Cryptology',
-          url: 'https://hackenproof.com/cryptology/cryptology',
-          archived: false
-        },
-        {
-          id: '/neverdie/neverdie-web',
-          name: 'Neverdie Web',
-          url: 'https://hackenproof.com/neverdie/neverdie-web',
-          archived: true
-        },
-        {
-          id: '/hacken-1/pentagon-hack',
-          name: 'Pentagon HACK',
-          url: 'https://hackenproof.com/hacken-1/pentagon-hack',
-          archived: true
-        },
-        {
-          id: '/ttc/ttc-mobile',
-          name: 'TTC | Mobile',
-          url: 'https://hackenproof.com/ttc/ttc-mobile',
-          archived: true
-        },
-        {
-          id: '/ttc/ttc-protocol',
-          name: 'TTC | Protocol',
-          url: 'https://hackenproof.com/ttc/ttc-protocol',
-          archived: true
-        },
-        {
-          id: '/ttc/ttc-sdk',
-          name: 'TTC | SDK',
-          url: 'https://hackenproof.com/ttc/ttc-sdk',
-          archived: true
-        },
-        {
-          id: '/tickets-travel-network/ttn-program',
-          name: 'TTN Program',
-          url: 'https://hackenproof.com/tickets-travel-network/ttn-program',
-          archived: true
-        },
-        {
-          id: '/unistake/unistake-smart-contracts',
-          name: 'Unistake Smart Contracts',
-          url: 'https://hackenproof.com/unistake/unistake-smart-contracts',
-          archived: true
-        },
-        {
-          id: '/vechain/vechainthor-vip191',
-          name: 'VeChainThor VIP191',
-          url: 'https://hackenproof.com/vechain/vechainthor-vip191',
-          archived: true
-        }
-      ]
+      [{id: '/hacken/hackenproof', name: 'HackenProof', url: 'https://hackenproof.com/hacken/hackenproof',
+        archived: false},
+       {id: '/kuna/kuna-crypto-exchange', name: 'Kuna Crypto Exchange',
+        url: 'https://hackenproof.com/kuna/kuna-crypto-exchange', archived: false},
+       {id: '/vechain/vechainthor', name: 'VeChainThor', url: 'https://hackenproof.com/vechain/vechainthor',
+        archived: false},
+       {id: '/vechain/vechainthor-wallet', name: 'VeChainThor Wallet',
+        url: 'https://hackenproof.com/vechain/vechainthor-wallet', archived: false},
+       {id: '/gate-dot-io/gate-dot-io-exchange', name: 'Gate.io Exchange',
+        url: 'https://hackenproof.com/gate-dot-io/gate-dot-io-exchange', archived: false},
+       {id: '/coingecko/coingecko', name: 'CoinGecko', url: 'https://hackenproof.com/coingecko/coingecko',
+        archived: false},
+       {id: '/p2pb2b/p2pb2b', name: 'P2PB2B', url: 'https://hackenproof.com/p2pb2b/p2pb2b',
+        archived: false},
+       {id: '/coinsbit/coinsbit', name: 'Coinsbit', url: 'https://hackenproof.com/coinsbit/coinsbit',
+        archived: false},
+       {id: '/hotbit/hotbit', name: 'Hotbit', url: 'https://hackenproof.com/hotbit/hotbit',
+        archived: false},
+       {id: '/whitebit/whitebit', name: 'WhiteBIT', url: 'https://hackenproof.com/whitebit/whitebit',
+        archived: false},
+       {id: '/vechain/vechainthor-vip191', name: 'VeChainThor VIP191',
+        url: 'https://hackenproof.com/vechain/vechainthor-vip191', archived: true}]
     )
   end
 
